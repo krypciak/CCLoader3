b69c478cac4b5d5023b636b76f3a9ebe3a0dbbdd
Alyxia Sother <alyxia@riseup.net>
2023-03-27 10:36:59 +0200

    Fix extension-preloader formatting

--- runtime/src/extension-preloader.ts
+++ runtime/src/extension-preloader.ts
@@ -2,17 +2,17 @@
 // Author: https://github.com/EL20202
 // https://github.com/EL20202/crosscode-extension-asset-preloader/blob/master/postload.js
 
-import { join } from "path";
-import { existsSync, readFileSync, readdirSync } from "fs";
+import { join } from 'path';
+import { existsSync, readFileSync, readdirSync } from 'fs';
 
-const extensionFolder = "assets/extension";
+const extensionFolder = 'assets/extension';
 
 let folders = readdirSync(extensionFolder);
 
 for (const extensionName of folders) {
   const filepath = join(extensionFolder, extensionName);
   if (existsSync(`${filepath}/${extensionName}.json`)) {
-    let data = JSON.parse(readFileSync(`${filepath}/${extensionName}.json`, { encoding: "utf-8" }));
+    let data = JSON.parse(readFileSync(`${filepath}/${extensionName}.json`, { encoding: 'utf-8' }));
 
     if (!data.files) break;
 
