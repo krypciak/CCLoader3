c3b26ed08e176279a429d7617bfbb39f8de92e5c
Alyxia Sother <alyxia@riseup.net>
2025-07-25 00:53:00 +0200

    Move game fixes to their own directory

--- packages/runtime/src/_postload.ts
+++ packages/runtime/src/_postload.ts
@@ -1,8 +1,8 @@
 import './font';
-import './font-race-condition-fix';
+import './fixes/font-race-condition-fix';
 import './options';
 import './input';
 
 import './ui/options';
 import './ui/version-display';
-import './extension-preloader';
+import './fixes/extension-preloader';
